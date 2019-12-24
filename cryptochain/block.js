a010313861039026e53af631735242ea851afb31
noonespecial009 <noonespecial009@gmail.com>
2019-12-23 20:49:09 -0800

    start block class

---
+++ cryptochain/block.js
@@ -0,0 +1,17 @@
+class Block {
+    constructor({timestamp, lastHash, hash, data}) {
+        this.timestamp = timestamp;
+        this.lastHash = lastHash;
+        this.data = data;
+        this.hash = hash;
+    }
+}
+
+const block1 = new Block({
+    timestamp: '01/01/01', 
+    lastHash: 'foo-lastHash', 
+    hash: 'foo-hash', 
+    data: 'foo-data'
+});
+
+console.log('block1', block1)
